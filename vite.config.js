071275e3114103d98bad508a1cc8873029e20113
Bemdoo Maor <bemdoo.maor1@gmail.com>
2023-10-13 21:46:10 +0100

    Removed default and added mine

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    minify: false,
+    terserOptions: {
+      compress: false,
+      mangle: false,
+    },
+    css: {
+      minify: false,
+    },
+  },
 })
