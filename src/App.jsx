1f443222d038d4c72457f137726d013e31fae969
Bemdoo Maor <bemdoo.maor1@gmail.com>
2023-10-15 16:27:58 +0100

    Worked on global styes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
 import Header from './components/Header/Header'
+import { GlobalStyles } from './styles/GlobalStyles'
 
 function App() {
 
   return (
     <>
+    <GlobalStyles/>
       <Header />
     </>
   )
---
+++ src/styles/GlobalStyles.jsx
@@ -0,0 +1,12 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyles = createGlobalStyle`
+    body{
+        margin: 0;
+        padding: 0;
+    }
+    #root{
+        width: 100vw;
+        height: 100vh;
+    }
+`
