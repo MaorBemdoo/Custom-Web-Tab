9318199a8b26ec452afce07d8c642a70c284c990
Bemdoo Maor <bemdoo.maor1@gmail.com>
2023-10-16 15:18:11 +0100

    Working on header structure

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,7 +2,15 @@ import React from 'react'
 
 const Header = ({className}) => {
   return (
-    <header className={className}>Heyo</header>
+    <header className={className}>
+      <nav>
+        <form target='_blank'>
+          <input type="search"/>
+          <input type="submit"/>
+        </form>
+      </nav>
+      <nav></nav>
+    </header>
   )
 }
 
--- src/styles/Header.style.jsx
+++ src/styles/Header.style.jsx
@@ -2,5 +2,5 @@ import styled from "styled-components";
 import Header from "../components/Header/Header";
 
 export const HeaderStyle = styled(Header)`
-    background-color: red;
+    
 `
