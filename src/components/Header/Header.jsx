2bb4f9defa393ceec5fce92fdbaa4ce80495eae5
Bemdoo Maor <bemdoo.maor1@gmail.com>
2023-10-17 17:05:42 +0100

    Finished nav1 logic

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import SearchIcon from '@mui/icons-material/Search'
+import { useState } from 'react'
 
 const Header = ({className}) => {
+  const [searchInput, setSearchInput] = useState('')
+  const [active, setActive] = useState(false)
+
   return (
     <header className={className}>
-      <nav className='active'>
-        <form target='_blank' action='https://google.com/search'>
-          <input type="search" placeholder="Search..." name="q" id="q"/>
+      <nav className={active ? "active" : ""}>
+        <form target='_blank' action='https://google.com/search' onFocus={() => setActive(true)} onMouseEnter={() => setActive(true)} onMouseLeave={() => setActive(false)}>
+          <input type="search" placeholder="Search..." name="q" id="q" value={searchInput} onChange={(e) => setSearchInput(e.target.value)}/>
           <button><SearchIcon/></button>
         </form>
       </nav>
