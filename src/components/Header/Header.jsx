bd672fc2eaafc69167a1279252fdbc519596fcaf
Bemdoo Maor <bemdoo.maor1@gmail.com>
2023-10-17 23:50:16 +0100

    Added an emptyInputHandler

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,10 +6,16 @@ const Header = ({className}) => {
   const [searchInput, setSearchInput] = useState('')
   const [active, setActive] = useState(false)
 
+  const emptyInputHan = (e) => {
+    if(searchInput.trim() == ""){
+      e.preventDefault()
+    };
+  }
+
   return (
     <header className={className}>
       <nav className={active ? "active" : ""}>
-        <form target='_blank' action='https://google.com/search' onMouseEnter={() => setActive(true)} onMouseLeave={() => setActive(false)}>
+        <form target='_blank' action='https://google.com/search' onMouseEnter={() => setActive(true)} onMouseLeave={() => setActive(false)} onSubmit={emptyInputHan}>
           <input type="search" placeholder="Search..." name="q" id="q" value={searchInput} onChange={(e) => setSearchInput(e.target.value)} onFocus={() => setActive(true)}/>
           <button><Search/></button>
         </form>
