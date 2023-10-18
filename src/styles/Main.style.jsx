93c0d1fdbc65a09073f8198c7964df1142ce169f
Bemdoo Maor <bemdoo.maor1@gmail.com>
2023-10-18 01:49:58 +0100

    Changed main tag from display grid to flex

--- src/styles/Main.style.jsx
+++ src/styles/Main.style.jsx
@@ -2,8 +2,11 @@ import styled from 'styled-components'
 import Main from '../components/Main'
 
 export const MainStyle = styled(Main)`
-    display: grid;
-    place-items: center;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    height: 80vh;
     & p:first-child{
         font-size: 10rem;
         font-weight: 500;
@@ -11,5 +14,8 @@ export const MainStyle = styled(Main)`
             position: absolute;
             font-size: 2rem;
         }
+    }
+    & p:nth-child(2){
+        font-size: 1.3rem;
     }
 `
