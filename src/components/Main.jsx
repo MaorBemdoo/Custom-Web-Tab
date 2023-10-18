af91fbd6301621ab4a4badc0938ac813e92175ab
Bemdoo Maor <bemdoo.maor1@gmail.com>
2023-10-18 02:08:54 +0100

    Markup of .socialIcons

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,10 +1,16 @@
 import React from "react";
+import {GitHub, LinkedIn, Twitter} from '@mui/icons-material'
 
 const Main = ({className}) => {
     return (
         <main className={className}>
             <p>4:17<sup>PM</sup></p>
             <p>Good morning, Bem.</p>
+            <div className="socialIcons">
+                <a href="https://github.com/MaorBemdoo"><GitHub fill="black"/></a>
+                <a href="https://www.linkedin.com/in/bemdoo-maor-449698279/"><LinkedIn fill="blue"/></a>
+                <a href="https://twitter.com/BemdooMaor"><Twitter fill="blue"/></a>
+            </div>
         </main>
     )
 }
