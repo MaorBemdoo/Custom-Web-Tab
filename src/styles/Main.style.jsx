1a049bdaaf9c9fae7f41501e6b7da7d9e1bb433e
Bemdoo Maor <bemdoo.maor1@gmail.com>
2023-10-18 01:39:46 +0100

    Finished styling time

--- src/styles/Main.style.jsx
+++ src/styles/Main.style.jsx
@@ -5,7 +5,11 @@ export const MainStyle = styled(Main)`
     display: grid;
     place-items: center;
     & p:first-child{
-        font-size: 3rem;
-        font-weight: 700;
+        font-size: 10rem;
+        font-weight: 500;
+        & sup{
+            position: absolute;
+            font-size: 2rem;
+        }
     }
 `
