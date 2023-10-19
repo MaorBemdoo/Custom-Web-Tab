0d5a43bbacf8ecda2cde5d486c4ef7c2ede3acc5
Bemdoo Maor <bemdoo.maor1@gmail.com>
2023-10-19 13:34:30 +0100

    added the mouseE func

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -21,7 +21,7 @@ const Header = ({className}) => {
   return (
     <header className={className}>
       <nav className={active ? "active" : ""}>
-        <form target='_blank' action='https://google.com/search' onMouseEnter={mouseE()} onMouseLeave={() => setActive(false)} onSubmit={emptyInputHan}>
+        <form target='_blank' action='https://google.com/search' onMouseEnter={mouseE} onMouseLeave={() => setActive(false)} onSubmit={emptyInputHan}>
           <input type="search" placeholder="Search..." name="q" id="q" value={searchInput} onChange={(e) => setSearchInput(e.target.value)} onFocus={() => setActive(true)}/>
           <button><Search/></button>
         </form>
