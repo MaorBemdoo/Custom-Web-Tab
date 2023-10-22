1de8b14985028438f2b61488b8d9f6241ed9be7c
Bemdoo Maor <bemdoo.maor1@gmail.com>
2023-10-22 23:33:25 +0100

    Worked on time

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import moment from "moment"
 
 const Main = ({className}) => {
     return (
         <main className={className}>
-            <p>4:17<sup>PM</sup></p>
+            <p>{moment().hour() > 12 ? moment().hour()-12 : moment().hour()}:{moment().minute()}<sup>{moment().hour() >= 12 ? "PM" : "AM"}</sup></p>
             <p>Thursday, 14th August, 2023</p>
             <p>Good morning, Bem.</p>
         </main>
