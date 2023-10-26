77948f053a04d556f9abbfda99a5278b596c2c65
Bemdoo Maor <bemdoo.maor1@gmail.com>
2023-10-26 10:36:38 +0100

    Made build.minify to false

--- vite.config.js
+++ vite.config.js
@@ -5,11 +5,11 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    minify: "terser",
-    terserOptions: {
-      compress: false,
-      mangle: false,
-    },
+    minify: false,
+    // terserOptions: {
+    //   compress: false,
+    //   mangle: false,
+    // },
     css: {
       minify: false,
     },
