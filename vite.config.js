5f8dbc4e7e0f0dbc932a9878097909ac1ca5634f
Bemdoo Maor <bemdoo.maor1@gmail.com>
2023-10-26 10:20:56 +0100

    Made build.minify to terser

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    minify: false,
+    minify: "terser",
     terserOptions: {
       compress: false,
       mangle: false,
