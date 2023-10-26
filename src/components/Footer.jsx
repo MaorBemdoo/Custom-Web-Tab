2270ed3ee200df50a75790898d915a1564784bcc
Bemdoo Maor <bemdoo.maor1@gmail.com>
2023-10-26 22:19:38 +0100

    Still working on quote

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,13 +1,31 @@
 import React from 'react'
 import {useState, useEffect} from 'react'
+import axios from 'axios'
 
 const Footer = ({className}) => {
 
     const [quote, setQuote] = useState('')
 
+    useEffect(() => {
+        axios.get(
+            "https://api.api-ninjas.com/v1/facts",{
+            headers: {
+                'X-Api-Key': 'Cy+7uXo9HGLpNBQ7gABM1w==8jZVKOP7GuIV73zT'
+            }}
+        )
+            .then(res => {
+                console.log(res.data[0].fact);
+                setQuote(res.data[0].fact)
+            })
+            .catch(err => {
+                // console.log(err.message);
+                setQuote(err.message)
+            })
+    }, [])
+
     return (
         <footer className={className}>
-            <p>{quote}</p>
+            <p>“{quote}”</p>
         </footer>
     )
 }
