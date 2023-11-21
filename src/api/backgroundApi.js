a6e7eff8fa86348a474a64ad920e76551b16a767
Bemdoo Maor <bemdoo.maor1@gmail.com>
2023-11-21 21:21:07 +0100

    Added an api folder and a screenshot

---
+++ src/api/backgroundApi.js
@@ -0,0 +1,5 @@
+export const backgroundApi = {
+    "morning": ["https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRA4DeXdUGmEHNV4cadPeH8TINUreQBvWePWw&usqp=CAU", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRAXeGaGu8HI1Dq93FmmATXcxjLd99dAHmFiA&usqp=CAU", "https://w0.peakpx.com/wallpaper/1014/56/HD-wallpaper-wonderful-rippling-lake-r-forest-autumn-ripples-r-clouds-lake.jpg"],
+    "afternoon": [],
+    "evening": []
+}
